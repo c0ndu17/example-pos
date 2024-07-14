bd68aa5003d379b43005ad3823994ca80db8c97c
George Phillips <georgephillips22@gmail.com>
2024-07-14 01:04:21 +0100

    Refactored shadcn setup

---
+++ packages/frontend/app/routes/login.tsx
@@ -0,0 +1,67 @@
+import { Link, NavLink } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+
+export const metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+import { Label } from "~/components/ui/label";
+
+let defaultEmail;
+
+if (process.env.NODE_ENV === "development") {
+  defaultEmail = "";
+}
+
+export default function LoginPage() {
+  const email = defaultEmail;
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link className="ml-auto inline-block text-sm underline disabled">
+                Forgot your password? Sorry, friend.
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
